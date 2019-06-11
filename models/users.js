368e95255ac122de0087aa53072a387b0ba5aba2
haidongNg <nguyenhaidong1412@gmail.com>
2019-06-11 20:24:12 +0700

    Create Models

--- models/users.js
+++ models/users.js
@@ -1,7 +1,23 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    email: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    
-})
+    fullName: {type: String, required: true},
+    userType: {type: String, required: true}, // tai xe or hang khach
+    phone: {type: Number, required: true, unique: true},
+    dateOfBirth: {type: Date, required: true},
+    registerDate: {
+        type: Date, default: new Date().getTime()
+    },
+    numberOfTrips: {type: Number},
+    numbeOfKms: {type: Number},
+    avatar: {type: String},
+    isActive: { type: Boolean, default: true},
+});
+
+
+const User = mongoose.model('User', UserSchema);
+module.exports = {
+    UserSchema, User
+}
