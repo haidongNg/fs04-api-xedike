3968864859deb0d24ad2cc8619640d392ddbd04a
haidongNg <nguyenhaidong1412@gmail.com>
2019-06-12 08:11:48 +0700

    Update

---
+++ api/users.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const {User} = require('../models/users');
+const router =express.Router();
+
+// route POST/user/register
+//desc register new user
+// accress Public
+
+router.post('/register', (req, res) => {
+    console.log(req.body);
+    const { email, password, fullName, userType, phone, dateOfBirth} = req.body;
+
+    const newUser = new User({
+        email, password, fullName, userType, phone, dateOfBirth
+    });
+    // newUser.save()
+    //     .then(user => res.status(200).json(user))
+    //     .cath(err => res.status(400).json(err))
+    newUser.save()
+            .then((user) => {res.status(200).json(user)})
+            .catch((err) => {res.status(400).json(err)})
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,9 +6,29 @@ const app = express();
 // my packages
 
 
-mongoose.connect('mongodb://localhost:27017/xedike', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost:27017/xedike', {useNewUrlParser: true, useCreateIndex: true})
     .then(() => console.log('Connected to DB'))
     .catch((err) => console.log(err));
+
+// Middleware
+// app.get('/', (rep, res, next) => {
+//     console.log('Now 1');
+//     next();
+// }, (rep, res, next) => {
+//     console.log('Now 2');
+//     next();
+// }, (rep, res, ) => {
+//     res.send('Hello world');
+//     // res.send('Hello world');
+// })
+
+// parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api/users', require('./api/users'));
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
