c0cdf290b3194c6c537fdd6f79f7422845adf8ad
haidongNg <nguyenhaidong1412@gmail.com>
2019-06-13 18:19:13 +0700

    Update file

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const {User} = require('../../models/users');
+const {User} = require('../../models/user');
 const router =express.Router();
 
-// route POST/user/register
-//desc register new user
-// accress Public
+// route    POST /api/users/register
+// desc     register new user
+// access   PUBLIC
 
 router.post('/register', (req, res) => {
     const { email, password, fullName, userType, phone, dateOfBirth} = req.body;
