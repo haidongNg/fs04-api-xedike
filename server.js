f107330ed6039cd0dac80b997f440e1633483a9c
haidongNg <nguyenhaidong1412@gmail.com>
2019-07-04 19:52:34 +0700

    connect database

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // mongodb+srv://xedikedb:IssVsBr6nFHnssDg@xedike-api-wtcsu.azure.mongodb.net/test?retryWrites=true&w=majority
 mongoose
-  .connect("mongodb://localhost:27017/xedike", {
+  .connect("mongodb+srv://adminfso4:dong@@..@cluster0-arkcd.azure.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useCreateIndex: true
   })
